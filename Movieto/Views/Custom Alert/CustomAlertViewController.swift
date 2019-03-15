b95e02052613771f630118ae3a027fbcd30984ba
Alireza <mr.toghyiani@icloud.com>
2019-03-15 13:08:58 +0330

    Make Custom Alert For Show Successful Search Result

---
+++ Movieto/Views/Custom Alert/CustomAlertViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CustomAlertViewController.swift
+//  Movieto
+//
+//  Created by ali on 3/15/19.
+//  Copyright © 2019 com.alireza. All rights reserved.
+//
+
+import UIKit
+
+class CustomAlertViewController: UIAlertController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+
+}
